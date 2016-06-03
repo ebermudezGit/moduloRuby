c28128639cf442b9a3261914e7d976ef38617bac
Eli Bermudez <bermudez.86@live.com.mx>
2016-06-03 00:52:41 -0500

    first commit

---
+++ desafioArray/desfioArray.rb
@@ -0,0 +1,19 @@
+#Desfio Array
+#optener un array con la suma de todos los elementos de la clave numeros, un array con la suma de cada uno de los elementos del array, la concatenacion de la cadena
+#ejemplo
+
+#my_class_hash = {
+#  "numeros:" [1,2,3,4,5],
+#  "arrays": [[1,2], [1,2,3], [1,2,3,4]],
+#  "strings": ['mi nombre es ', 'Oscar ', 'y tengo']
+#}
+
+#resultado esperado
+#[15, [3, 6, 10] ,"mi nombre es Oscar y tengo"]
+
+hash = {
+  "numeros":[1,2,3,4,5],
+  "arrays": [[1,2], [1,2,3], [1,2,3,4]],
+  "strings":['mi nombre es ', 'Oscar ', 'y tengo']
+}
+
