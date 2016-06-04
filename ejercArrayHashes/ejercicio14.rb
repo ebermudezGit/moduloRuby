6370cc100a855c24a7c7ed8ac9198f3b96893286
Eli Bermudez <bermudez.86@live.com.mx>
2016-06-04 00:20:08 -0500

    terminando la 14

--- ejercArrayHashes/ejercicio14.rb
+++ ejercArrayHashes/ejercicio14.rb
@@ -1,7 +1,7 @@
 #Ejerciicio 14 de Ruby Array y Hashes 
 
 myArray =  [[2,3],[3,4],[3,3]]
-myResultado = Array.new()
+myResultado = Array.new
 arrbufer = Array.new()
 
 3.times do |index|
@@ -16,9 +16,13 @@ arrbufer = Array.new()
 end
 
 myArray.each_with_index  do |value,index|
-	if myResultado[index] == value
+	if myResultado[index] === value
 		puts "Atinaste #{myResultado[index]} son iguales #{value}"
 	end
 end
-print myResultado
-puts
+
+myResultado = Array.new
+3.times{myResultado<<[Random.rand(0..5),Random.rand(0..5)]}
+
+print "El arreglo aleatorio es #{myResultado}"
+puts 
