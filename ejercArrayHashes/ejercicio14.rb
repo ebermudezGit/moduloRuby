a344c0c2fe7fa32811893c8d9c5e18b2092bf190
Eli Bermudez <bermudez.86@live.com.mx>
2016-06-03 22:04:32 -0500

    mas cambios 14

---
+++ ejercArrayHashes/ejercicio14.rb
@@ -0,0 +1,24 @@
+#Ejerciicio 14 de Ruby Array y Hashes 
+
+myArray =  [[2,3],[3,4],[3,3]]
+myResultado = Array.new()
+arrbufer = Array.new()
+
+3.times do |index|
+	puts "dame el par #{index+1}"
+	puts "dame primero"
+	arrbufer<<gets.chomp.to_i
+	puts "dame Segundo"
+	arrbufer<<gets.chomp.to_i
+
+	myResultado<<arrbufer
+	arrbufer = Array.new
+end
+
+myArray.each_with_index  do |value,index|
+	if myResultado[index] == value
+		puts "Atinaste #{myResultado[index]} son iguales #{value}"
+	end
+end
+print myResultado
+puts
